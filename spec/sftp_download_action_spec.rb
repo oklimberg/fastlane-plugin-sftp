d292f02724dc6829c2b9b3ffd63ca23a899927b8
Oliver Limberg <oklimberg@gmail.com>
2019-04-05 23:38:41 +0200

    additional tests

--- spec/sftp_download_action_spec.rb
+++ spec/sftp_download_action_spec.rb
@@ -31,5 +31,50 @@ describe Fastlane::Actions::SftpDownloadAction do
       expect(File).to exist("down/sub_folder/file_03.txt")
       expect(File).to exist("down/sub_folder/file_04.txt")
     end
+
+    it 'downloads files inside a Fastlane file 2' do
+      expect do
+        Fastlane::FastFile.new.parse("lane :test do
+          sftp_download(
+            server_url: 'sftp.server.example',
+            server_user: 'sftp_test',
+            server_password: 'password',
+            server_key: '../assets/keys/valid_key_no_pass',
+            target_dir: 'down',
+            file_paths: ['download/file_01.txt', 'download/file_02.txt', 'download/file_does_not_exist.txt', 'download/sub_folder']
+          )
+        end").runner.execute(:test)
+      end.to raise_error("You can't use 'password' and 'server_key' options in one run.")
+    end
+
+    it 'downloads files inside a Fastlane file 3' do
+      expect do
+        Fastlane::FastFile.new.parse("lane :test do
+          sftp_download(
+            server_url: 'sftp.server.example',
+            server_user: 'sftp_test',
+            server_key: '../assets/keys/valid_key_no_pass',
+            server_password: 'password',
+            target_dir: 'down',
+            file_paths: ['download/file_01.txt', 'download/file_02.txt', 'download/file_does_not_exist.txt', 'download/sub_folder']
+          )
+        end").runner.execute(:test)
+      end.to raise_error("You can't use 'server_key' and 'password' options in one run.")
+    end
+
+    it 'downloads files inside a Fastlane file 4' do
+      expect do
+        Fastlane::FastFile.new.parse("lane :test do
+          sftp_download(
+            server_url: 'sftp.server.example',
+            server_user: 'sftp_test',
+            server_key: '/assets/keys/valid_key_no_pass',
+            server_password: 'password',
+            target_dir: 'down',
+            file_paths: ['download/file_01.txt', 'download/file_02.txt', 'download/file_does_not_exist.txt', 'download/sub_folder']
+          )
+        end").runner.execute(:test)
+      end.to raise_error("Failed to load RSA key... /assets/keys/valid_key_no_pass")
+    end
   end
 end
