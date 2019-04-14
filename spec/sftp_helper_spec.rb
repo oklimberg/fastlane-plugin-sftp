c999e3878fc7ddb807f7c251019b140d1f860a7b
Oliver Limberg <o.limberg@portrix.net>
2019-04-14 16:31:29 +0200

    fixed existing tests after adding 'port' option

--- spec/sftp_helper_spec.rb
+++ spec/sftp_helper_spec.rb
@@ -16,40 +16,40 @@ describe Fastlane::Helper::SftpHelper do
   describe '#login' do
     it 'raise error because of missing host' do
       expect do
-        Fastlane::Helper::SftpHelper.login(nil, "user", "password", nil, nil)
+        Fastlane::Helper::SftpHelper.login(nil, nil, "user", "password", nil, nil)
       end.to raise_exception('server_url, server_user and server_password or server_key must be set')
     end
 
     it 'raise error because of missing user' do
       expect do
-        Fastlane::Helper::SftpHelper.login("host", nil, "password", nil, nil)
+        Fastlane::Helper::SftpHelper.login("host", nil, nil, "password", nil, nil)
       end.to raise_exception('server_url, server_user and server_password or server_key must be set')
     end
 
     it 'raise error because of missing password and RSA key file path' do
       expect do
-        Fastlane::Helper::SftpHelper.login("host", "user", nil, nil, nil)
+        Fastlane::Helper::SftpHelper.login("host", nil, "user", nil, nil, nil)
       end.to raise_exception('server_url, server_user and server_password or server_key must be set')
     end
 
     it 'succeed with user & password' do
-      sftp = Fastlane::Helper::SftpHelper.login("sftp.server.example", "sftp_test", "password", nil, nil)
+      sftp = Fastlane::Helper::SftpHelper.login("sftp.server.example", nil, "sftp_test", "password", nil, nil)
       expect(sftp).not_to(be_nil)
     end
 
     it 'succeed with user & RSA key file without pass' do
-      sftp = Fastlane::Helper::SftpHelper.login("sftp.server.example", "sftp_test", nil, "assets/keys/valid_key_no_pass", nil)
+      sftp = Fastlane::Helper::SftpHelper.login("sftp.server.example", nil, "sftp_test", nil, "assets/keys/valid_key_no_pass", nil)
       expect(sftp).not_to(be_nil)
     end
 
     it 'succeed with user & RSA key file with pass' do
-      sftp = Fastlane::Helper::SftpHelper.login("sftp.server.example", "sftp_test", nil, "assets/keys/valid_key_with_pass", "passphrase")
+      sftp = Fastlane::Helper::SftpHelper.login("sftp.server.example", nil, "sftp_test", nil, "assets/keys/valid_key_with_pass", "passphrase")
       expect(sftp).not_to(be_nil)
     end
 
     it 'fail with user & RSA key file with pass' do
       expect do
-        Fastlane::Helper::SftpHelper.login("sftp.server.example", "sftp_test", nil, "assets/keys/valid_key_with_pass", "wrong")
+        Fastlane::Helper::SftpHelper.login("sftp.server.example", nil, "sftp_test", nil, "assets/keys/valid_key_with_pass", "wrong")
       end.to(raise_exception("Decrypt failed on private key"))
     end
   end
